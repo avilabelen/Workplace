e13e911e730b5f12c04ed7f22b5737374b99c5ae
Belen Avila <alexandravila4@gmail.com>
2025-02-28 23:27:34 -0500

    Changes

--- mis_proyectos/Challenge.cpp
+++ mis_proyectos/Challenge.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <set>
+
+
 void displayInventory(const std::map<std::string, int>& inventory) {
     std::cout << "Current Inventory:\n";
     for (const auto& item : inventory) {
@@ -8,29 +11,69 @@ void displayInventory(const std::map<std::string, int>& inventory) {
     }
     std::cout << std::endl;
 }
-void addnewproduct(map<string,int>&inventory)
+
+void addnewproduct(std::map<std::string, int>& inventory) { 
+    std::string newproduct;
+    int quantity;
+    std::cout << "Enter the product name: ";
+    std::cin >> newproduct;
+    std::cout << "Enter the quantity: ";
+    std::cin >> quantity;
+
+    inventory.insert({newproduct, quantity});
+}
+
+void addnewcategory(std::set<std::string>& productCategories) { 
+    std::string newcategory;
+    std::cout << "Enter the new category name: ";
+    std::cin >> newcategory;
+    productCategories.insert(newcategory);
+
+    std::cout << "Category added: " << newcategory << std::endl;
+}
 
 int main() {
     std::map<std::string, int> inventory;
 
-    // Adding products
+    //Adding products
     inventory["Laptop"] = 5;
     inventory["Mouse"] = 20;
     inventory["Keyboard"] = 10;
 
+    
+    addnewproduct(inventory); 
+     
 
-    inventory.insert({"CPU",8});//Add a new product
-    inventory["Mouse"]=30; // Update the quantity of an existing product.
+    //change the values of some product
+    inventory["Mouse"] = 30; 
+
 
     displayInventory(inventory);
-    std::cout << "All available products : \n";
+
+   
+    std::cout << "All available products: \n";
     for (const auto& item : inventory) {
-        std::cout << item.first << "--";
+        std::cout << item.first << " -- ";
     }
+    std::cout << std::endl;
 
-
-    return 0;
-}
 
 
 //TASK02
+    std::set<std::string> productCategories;
+
+    // Adding categories
+    productCategories.insert("Electronics");
+    productCategories.insert("Accessories");
+    productCategories.insert("Peripherals");
+
+
+    addnewcategory(productCategories);
+
+    std::cout << "Product Categories:\n";
+    for (const auto& category : productCategories) {
+        std::cout << "- " << category << "\n";
+    }
+
+    return 0;
+}
